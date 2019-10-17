a44391d41ae35666fcd32dfa61b7f74f97a753d3
alexsonnysar <56088949+alexsonnysar@users.noreply.github.com>
2019-10-16 21:57:08 -0500

    Connected main menu and pause menu

--- SpaceShooter/Assets/Scripts/PauseMenu/PauseMenu.cs
+++ SpaceShooter/Assets/Scripts/PauseMenu/PauseMenu.cs
@@ -5,14 +5,50 @@ using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    public void PlayGame()
+    public static bool GameIsPaused = false;
+
+    public GameObject PauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Debug.Log("Loading Main Menu...");
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 
     public void QuitGame()
     {
-        Debug.Log("QUIT!");
+        Debug.Log("Quitting game...");
         Application.Quit();
     }
 }
