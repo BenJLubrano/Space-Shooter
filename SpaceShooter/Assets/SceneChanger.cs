c6f9e245eb23ba5ee559633bd91b571a18c1b23c
Ricardo J. Useche <46338889+ricardouseche@users.noreply.github.com>
2019-12-07 19:14:29 -0600

    Added scene transporter for colliding with a planet
    Usage: On the prefab, enter scene you wanna go to in "transport to" variable on the transport menu

---
+++ SpaceShooter/Assets/SceneChanger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public string transportTo;
+    public GameObject Player;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Player = GameObject.FindWithTag("Player");
+    }
+
+    public void transportPlayer()
+    {
+        SceneManager.LoadScene(transportTo);
+    }
+}
