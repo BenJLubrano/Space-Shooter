bb20b8cf57863d57fd23989e97cfebbea60ac755
BenJLubrano <benjaminlubrano@gmail.com>
2020-01-05 22:40:10 -0600

    spawner changes/groundwork for boss3

---
+++ SpaceShooter/Assets/Scripts/Enemy/Boss3Controller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss3Controller : BossController
+{
+
+    Vector2 leftShipSpawner;
+    Vector2 rightShipSpawner;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
